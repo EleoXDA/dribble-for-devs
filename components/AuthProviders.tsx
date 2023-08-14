0fbba89e960e001c128db6de4de9eb8c8f053a5d
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-14 23:20:08 +0200

    update Auth logic to get Google Provider access

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client"
+import { getProviders, signIn } from "next-auth/react"
+import { type } from "os";
+import { useState, useEffect } from "react"
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    }
+    fetchProviders();
+  }, [])
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider : Provider, i) => (
+          <button key={i} onClick={()=>signIn(provider?.id)}>{provider.id}</button>
+      ))}
+      </div>
+    )
+  
   return (
     <div>AuthProviders</div>
   )
-}
+}}
 
 export default AuthProviders
