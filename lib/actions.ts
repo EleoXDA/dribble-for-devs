f17514761fb0acedf6aad1eecb8693d0a91114ed
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 00:38:38 +0200

    add apikey for getting/creating user

--- lib/actions.ts
+++ lib/actions.ts
@@ -17,10 +17,12 @@ const makeGraphQLRequest = async (query:string, variables = {}) => {
 }
 
 export const getUser = (email:string) => {
+  client.setHeader("x-api-key", apiKey);
   return makeGraphQLRequest(getUserQuery, { email });
 }
 
 export const createUser = (name:string, email:string, avatarUrl:string) => {
+  client.setHeader("x-api-key", apiKey);
   const variables = { input: { name, email, avatarUrl } }
   return makeGraphQLRequest(createUserMutation, variables);
 }
