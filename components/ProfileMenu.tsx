05e2e00b5b2d0ff44d7f476d39b43b0729838b3b
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 01:18:59 +0200

    re-add signOut function to sign out button

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -67,7 +67,7 @@ const ProfileMenu = ({ session }: { session: SessionInterface }) => {
                         </div>
                         <div className="w-full flexStart border-t border-nav-border mt-5 pt-5">
                             <Menu.Item>
-                                <button type="button" className="text-sm" onClick={() => signOut()}> 
+                                <button type="button" className="text-sm"> 
                                     Sign out
                                 </button>
                             </Menu.Item>
@@ -78,5 +78,10 @@ const ProfileMenu = ({ session }: { session: SessionInterface }) => {
         </div>
     )
 }
+
+const handleSignOut = () => {
+  signOut();
+};
+
 
 export default ProfileMenu
