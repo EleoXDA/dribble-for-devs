69c808d765c16a871c102a765606141ceabe805d
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 00:38:10 +0200

    update access rule to users and projects

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+//@ts-ignore
 const User = g.model('User', {
   name: g.string().length({min:2, max:20}),
   email: g.string().unique(),
@@ -8,8 +9,9 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
-})
+}).auth((rules)=> {rules.public().read()})
 
+//@ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({min:3}),
   description: g.string(),
@@ -18,9 +20,17 @@ const Project = g.model('Project', {
   githubUrl: g.url().optional(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
+}).auth((rules)=> {rules.public().read(), rules.private().create().update().delete()})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET'),
 })
 
-
 export default config({
-  schema: g
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  }
 })
