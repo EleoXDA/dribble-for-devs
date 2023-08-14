232499d9c71ac3b80ea7c4b2ea9798c786c96b29
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-14 20:39:12 +0200

    Add Navbar menus and titles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { NavLinks } from '@/constants'
+import AuthProviders from './AuthProviders'
+
 
 const Navbar = () => {
+  const session = null;
   return (
     <nav className='flexBetween navbar'>
-      <div>Navbar</div>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image src='./logo.svg' width={115} height={43} alt='DevDribble'/>
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className='flexCenter gap-4'>
+            {session ? (
+              <>
+                UserPhoto
+                <Link href='/create-project'>
+                  Share Work
+                </Link>
+              </>
+            ) : (
+              <AuthProviders />
+            )}
+      </div>
     </nav>
   )
 }
