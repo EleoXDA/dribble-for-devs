c8a419f92a21652af351d986e229585a00665a63
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 21:32:22 +0200

    update Button and reformat code

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,9 @@
-"use client"
-import { getProviders, signIn } from "next-auth/react"
-import { type } from "os";
-import { useState, useEffect } from "react"
+'use client';
+import { getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+
+import Button from './Button';
+
 
 type Provider = {
   id: string;
@@ -10,7 +12,7 @@ type Provider = {
   signinUrl: string;
   callbackUrl: string;
   signinUrlParams?: Record<string, string> | null;
-}
+};
 
 type Providers = Record<string, Provider>;
 
@@ -21,22 +23,19 @@ const AuthProviders = () => {
     const fetchProviders = async () => {
       const res = await getProviders();
       setProviders(res);
-    }
+    };
     fetchProviders();
-  }, [])
+  }, []);
 
   if (providers) {
     return (
       <div>
-        {Object.values(providers).map((provider : Provider, i) => (
-          <button key={i} onClick={()=>signIn(provider?.id)}>{provider.id}</button>
+      {Object.values(providers).map((provider: Provider, i) => (
+          <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
       ))}
-      </div>
-    )
-  
-  return (
-    <div>AuthProviders</div>
-  )
-}}
+  </div>
+    );
+  }
+};
 
-export default AuthProviders
+export default AuthProviders;
