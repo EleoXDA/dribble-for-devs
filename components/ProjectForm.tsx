72259be3edf30401a997c76433d42c0c10110cbc
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-16 00:09:57 +0200

    update placeholder text on project add/edit pages

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -120,7 +120,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
             <FormField
                 title='Description'
                 state={form.description}
-                placeholder="Showcase and discover remarkable developer projects."
+                placeholder="Showcase your project"
                 isTextArea
                 setState={(value) => handleStateChange('description', value)}
             />
@@ -129,7 +129,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
                 type="url"
                 title="Website URL"
                 state={form.liveSiteUrl}
-                placeholder="https://jsmastery.pro"
+                placeholder="Please paste URL of deployed page starting with https://"
                 setState={(value) => handleStateChange('liveSiteUrl', value)}
             />
 
@@ -137,7 +137,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
                 type="url"
                 title="GitHub URL"
                 state={form.githubUrl}
-                placeholder="https://github.com/adrianhajdin"
+                placeholder="Please paste URL of GitHub project starting with https://"
                 setState={(value) => handleStateChange('githubUrl', value)}
             />
 
