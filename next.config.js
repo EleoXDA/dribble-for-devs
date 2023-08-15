a1dde38e8019ba543c5352e00645a062dea0a5cf
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 21:33:18 +0200

    add experimental config for cloudinary + graphql

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['cloudinary', 'graphql-request'],
   }
 }
 
