0b6bcf50911375d84fc6d0c67aef9e06c5410811
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-15 21:29:28 +0200

    add more static data

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,6 +1,112 @@
-export const getUserQuery =  `
+export const createProjectMutation = `
+	mutation CreateProject($input: ProjectCreateInput!) {
+		projectCreate(input: $input) {
+			project {
+				id
+				title
+				description
+				createdBy {
+					email
+					name
+				}
+			}
+		}
+	}
+`;
+
+export const updateProjectMutation = `
+	mutation UpdateProject($id: ID!, $input: ProjectUpdateInput!) {
+		projectUpdate(by: { id: $id }, input: $input) {
+			project {
+				id
+				title
+				description
+				createdBy {
+					email
+					name
+				}
+			}
+		}
+	}
+`;
+
+export const deleteProjectMutation = `
+  mutation DeleteProject($id: ID!) {
+    projectDelete(by: { id: $id }) {
+      deletedId
+    }
+  }
+`;
+
+export const createUserMutation = `
+	mutation CreateUser($input: UserCreateInput!) {
+		userCreate(input: $input) {
+			user {
+				name
+				email
+				avatarUrl
+				description
+				githubUrl
+				linkedInUrl
+				id
+			}
+		}
+	}
+`;
+
+export const projectsQuery = `
+  query getProjects($category: String, $endCursor: String) {
+    projectSearch(first: 8, after: $endCursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getProjectByIdQuery = `
+  query GetProjectById($id: ID!) {
+    project(by: { id: $id }) {
+      id
+      title
+      description
+      image
+      liveSiteUrl
+      githubUrl
+      category
+      createdBy {
+        id
+        name
+        email
+        avatarUrl
+      }
+    }
+  }
+`;
+
+export const getUserQuery = `
   query GetUser($email: String!) {
-    user(by: {email: $email}) {
+    user(by: { email: $email }) {
       id
       name
       email
@@ -10,21 +116,27 @@ export const getUserQuery =  `
       linkedInUrl
     }
   }
+`;
 
-`
-
-export const createUserMutation = `
-  mutation CreateUser($input: UserCreateInput!) {
-    userCreate(input: $input) {
-      user {
-        id
-        name
-        email
-        avatarUrl
-        description
-        githubUrl
-        linkedInUrl
+export const getProjectsOfUserQuery = `
+  query getUserProjects($id: ID!, $last: Int = 4) {
+    user(by: { id: $id }) {
+      id
+      name
+      email
+      description
+      avatarUrl
+      githubUrl
+      linkedInUrl
+      projects(last: $last) {
+        edges {
+          node {
+            id
+            title
+            image
+          }
+        }
       }
     }
   }
-`
+`;
