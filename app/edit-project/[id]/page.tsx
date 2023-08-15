8517dba4ae53dbaddb36064f71c2349419be50c0
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-16 00:07:06 +0200

    add project edit modal

---
+++ app/edit-project/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from "next/navigation";
+
+import Modal from "@/components/Modal";
+import ProjectForm from "@/components/ProjectForm";
+import { getCurrentUser } from "@/lib/session";
+import { getProjectDetails } from "@/lib/actions";
+import { ProjectInterface } from "@/common.types";
+
+const EditProject = async ({ params: { id } }: { params: { id: string } }) => {
+  const session = await getCurrentUser();
+
+  if (!session?.user) redirect("/")
+
+  const result = await getProjectDetails(id) as { project?: ProjectInterface };
+  
+  if (!result?.project) return (
+    <p className="no-result-text">Failed to fetch project info</p>
+  )
+
+  return (
+    <Modal>
+      <h3 className="modal-head-text">Edit Project</h3>
+
+      <ProjectForm type="edit" session={session} project={result?.project} />
+    </Modal>
+  );
+};
+
+export default EditProject;
