7aae91c822ab635fa9ae0435c84d7d73af67c239
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-16 20:04:18 +0200

    update session.ts + console.log to detect token

--- lib/session.ts
+++ lib/session.ts
@@ -1,11 +1,11 @@
-import { getServerSession } from "next-auth/next";
-import { NextAuthOptions } from "next-auth";
-import type { User as AdapterUser, User } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import jsonwebtoken from "jsonwebtoken";
-import { JWT } from "next-auth/jwt";
-import { SessionInterface, UserProfile } from "../common.types";
-import { createUser, getUser } from "./actions";
+import { getServerSession } from 'next-auth/next';
+import { NextAuthOptions, User } from 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
+import GoogleProvider from 'next-auth/providers/google';
+import jsonwebtoken from 'jsonwebtoken';
+import { JWT } from 'next-auth/jwt';
+import { SessionInterface, UserProfile } from '../common.types';
+import { createUser, getUser } from './actions';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -15,59 +15,72 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   jwt: {
-    encode: ({secret, token}) => {
-      const encodedToken = jsonwebtoken.sign({
-        ...token
-        , iss: 'grafbase',
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 60}, secret)
-      return encodedToken;
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign(
+        {
+          ...token,
+          iss: 'grafbase',
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        secret
+      );
 
+      return encodedToken;
     },
-    decode: ({secret, token}) => {
-      const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
-      return decodedToken;
+    decode: async ({ secret, token }) => {
+      const decodedToken = jsonwebtoken.verify(token!, secret);
+      console.log(decodedToken);
+      return decodedToken as JWT;
     },
   },
   theme: {
-    colorScheme: "light",
-    logo: "../public/logo.svg",
+    colorScheme: 'light',
+    logo: '../public/logo.svg',
   },
   callbacks: {
-    async session({session}) {
+    async session({ session }) {
       const email = session?.user?.email as string;
       try {
-        const data = await getUser(email) as { user?: UserProfile };
+        const data = (await getUser(email)) as { user?: UserProfile };
+
         const newSession = {
           ...session,
           user: {
             ...session.user,
-            ...data.user,
-          }
-        }
+            ...data?.user,
+          },
+        };
+
         return newSession;
-      } catch (error) {
-        console.log(error);
-        return session
+      } catch (error: any) {
+        console.error('Error retrieving user data: ', error.message);
+        return session;
       }
     },
-    async signIn({user} : {user: AdapterUser | User }) {
+    async signIn({ user }: { user: AdapterUser | User }) {
       try {
-        const userExists = await getUser(user?.email as string) as { user?: UserProfile };
+        const userExists = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
+
         if (!userExists.user) {
-          await createUser(user.name as string, user.email as string, user.image as string);
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          );
         }
+
         return true;
-      } catch (error:any) {
-        console.log(error);
+      } catch (error: any) {
+        console.log('Error checking if user exists: ', error.message);
         return false;
-      };
+      }
     },
-  }
-}
+  },
+};
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions) as SessionInterface;
+  const session = (await getServerSession(authOptions)) as SessionInterface;
   return session;
 }
-    
-
